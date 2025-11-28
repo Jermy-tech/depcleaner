02371828e257c63c16450281c2ab9267a861d78e
Jermy <penajermy24@gmail.com>
2025-11-28 13:54:14 -0500

    Update __init__.py

--- depcleaner/__init__.py
+++ depcleaner/__init__.py
@@ -3,5 +3,6 @@ from depcleaner.core import DepCleaner
 from depcleaner.scanner import Scanner
 from depcleaner.fixer import Fixer
 
-__version__ = "1.3.3"
+__version__ = "1.4.0"
 __all__ = ["DepCleaner", "Scanner", "Fixer"]
+
